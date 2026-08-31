98c7a586b7eae14085f83fd73fd12c16b6c09cdb
Vanessa Pöhl <vanessapoehl285@gmail.com>
2026-08-31 21:59:15 +0200

    Datenbank-Setup für lokale Ausführung optimiert

--- student_info.sh
+++ student_info.sh
@@ -3,7 +3,7 @@
 
 echo -e "\n~~ My Computer Science Students ~~\n"
 
-PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+PSQL="psql -X --username=postgres --dbname=students --no-align --tuples-only -c"
 
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
